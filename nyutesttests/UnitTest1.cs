c6237f54678186f40e846708ca4475fb22cf5c59
Robert Harmon <harmonr@gmail.com>
2023-12-20 11:57:20 -0500

    update.

--- nyutesttests/UnitTest1.cs
+++ nyutesttests/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace nyutesttests
 
         //This test is designed to fail to show the CI/CD pipeline working.
         [Theory]
-        [InlineData("https:/www.google.com")]
+        [InlineData("https://www.google.com")]
         public void TestMalformedURI(string uri) {
             new Uri(uri);
         }
